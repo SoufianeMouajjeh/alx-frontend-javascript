3f950f9f31ada7f84abb53ecf0cc5963aa7bd0f9
SoufianeMouajjeh <melmouajjeh@gmail.com>
2024-06-24 16:52:59 +0100

    done

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   const report = {
     allEmployees: employeesList,
     getNumberOfDepartments(employeesList) {
-    return Object.keys(employeesList).length;
+      return Object.keys(employeesList).length;
     },
   };
   return report;
